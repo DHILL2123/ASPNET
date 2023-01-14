4ac8a8f862fc166839bd3ccf124e43bb46a1ccdc
DeAdrien Hill <deadrienhill@yahoo.com>
2023-01-14 12:15:30 -0600

    completed Insert Product

--- Testing/Models/Product.cs
+++ Testing/Models/Product.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Testing.Models
 {
 	public class Product
@@ -9,7 +11,7 @@ namespace Testing.Models
 		public int CategoryID { get; set; }
 		public int OnSale { get; set; }
 		public int StockLevel { get; set; }
-
+		public IEnumerable<Category> Categories { get; set; }
 
 		public Product()
 		{
